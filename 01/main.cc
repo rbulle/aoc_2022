90a838eec241a325492e3b64e0b34677051ca93e
Raphaël Bulle <rbulle@tutanota.com>
2022-12-02 08:25:05 -0500

    Solution to second pbm of day 1.

--- 01/main.cc
+++ 01/main.cc
@@ -8,8 +8,12 @@ int main()
 {
     string line;
     int currentElfCalories = 0;
-    int maxElfCalories = 0;
-    int elfNumber = 0;
+    int firstMaxElfCalories = 0;
+    int secondMaxElfCalories = 0;
+    int thirdMaxElfCalories = 0;
+    int firstElfNumber = 0;
+    int secondElfNumber = 0;
+    int thirdElfNumber = 0;
     int index = 0;
     int calories = 0;
     ifstream dataFile("./data.txt");
@@ -18,10 +22,30 @@ int main()
     {
         if (line.empty())
         {
-            if (currentElfCalories > maxElfCalories)
+            if (currentElfCalories >          thirdMaxElfCalories &&
+                currentElfCalories > secondMaxElfCalories &&
+                currentElfCalories >
+                firstMaxElfCalories)
             {
-                maxElfCalories = currentElfCalories;
-                elfNumber = index;
+                thirdMaxElfCalories = secondMaxElfCalories;
+                secondMaxElfCalories = firstMaxElfCalories;
+                firstMaxElfCalories = currentElfCalories;
+                thirdElfNumber = secondElfNumber;
+                secondElfNumber = firstElfNumber;
+                firstElfNumber = index;
+            }
+            else if (currentElfCalories > thirdMaxElfCalories &&
+            currentElfCalories > secondMaxElfCalories)
+            {
+                thirdMaxElfCalories = secondMaxElfCalories;
+                secondMaxElfCalories = currentElfCalories;
+                thirdElfNumber = secondElfNumber;
+                secondElfNumber = index;
+            }
+            else if (currentElfCalories > thirdMaxElfCalories)
+            {
+                thirdMaxElfCalories = currentElfCalories;
+                thirdElfNumber = index;
             }
             currentElfCalories = 0;
             ++index;
@@ -29,11 +53,39 @@ int main()
         else
         {
             calories = stoi(line);
+            cout << calories << "\n";
             currentElfCalories += calories;
         }
     }
-    cout << "Elf carrying the most calories is Elf nb: " << elfNumber << "\n";
-    cout << "This elf carries " << maxElfCalories << " calories in total." << "\n";
+    if (currentElfCalories > thirdMaxElfCalories &&
+        currentElfCalories > secondMaxElfCalories &&
+        currentElfCalories > firstMaxElfCalories)
+    {
+        thirdMaxElfCalories = secondMaxElfCalories;
+        secondMaxElfCalories = firstMaxElfCalories;
+        firstMaxElfCalories = currentElfCalories;
+        thirdElfNumber = secondElfNumber;
+        secondElfNumber = firstElfNumber;
+        firstElfNumber = index;
+    }
+    else if (currentElfCalories > thirdMaxElfCalories &&
+    currentElfCalories > secondMaxElfCalories)
+    {
+        thirdMaxElfCalories = secondMaxElfCalories;
+        secondMaxElfCalories = currentElfCalories;
+        thirdElfNumber = secondElfNumber;
+        secondElfNumber = index;
+    }
+    else if (currentElfCalories > thirdMaxElfCalories)
+    {
+        thirdMaxElfCalories = currentElfCalories;
+        thirdElfNumber = index;
+    }
+    cout << "The elf carrying the most calories is Elf nb: " << firstElfNumber << " with " << firstMaxElfCalories << " calories in total." << "\n";
+    cout << "The second elf carrying the most calories is Elf nb: " << secondElfNumber << " with " << secondMaxElfCalories << " calories in total." << "\n";
+    cout << "The third elf carrying the most calories is Elf nb: " << thirdElfNumber << " with " << thirdMaxElfCalories << " calories in total." << "\n";
+    int totalMaxCalories = firstMaxElfCalories + secondMaxElfCalories + thirdMaxElfCalories;
+    cout << "These three elves carry " << totalMaxCalories << " calories in total." << "\n";
     dataFile.close();
 
     return 0;
